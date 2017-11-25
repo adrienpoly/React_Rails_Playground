bdad105a785a166f1ec72e5919d2fe28f941f0bf
Adrien Poly <adrienpoly@gmail.com>
2017-11-25 18:10:28 +0000

    use https for openweather api call

--- app/javascript/weather/actions/index.js
+++ app/javascript/weather/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API_KEY = "f360afe42cb97f6789e4f07b6b24ed51"
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
