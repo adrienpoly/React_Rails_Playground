e5546c26ec2a8a256850650e95e80154bf3984d1
Adrien Poly <adrienpoly@gmail.com>
2017-11-25 03:53:02 +0000

    optimisations for page speed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,11 +22,13 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.serve_static_assets = true ##added
+  config.public_file_server.headers = {
+    'Cache-Control' => 'public, max-age=31536000'
+  }
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
