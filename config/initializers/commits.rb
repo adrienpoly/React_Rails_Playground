e573c26ab1013bee08430b8a20f3c6b1e6f0a0cc
Adrien Poly <adrienpoly@gmail.com>
2017-11-26 00:12:57 +0000

    Correct the test url

--- config/initializers/commits.rb
+++ config/initializers/commits.rb
@@ -1,8 +1,7 @@
 # for each new commit log the page speed results in the DB for performances stats
 # you must run first heroku labs:enable runtime-dyno-metadata to get ENV metadata
 
-# TEST_URL = 'https://react-rails-playground.herokuapp.com'
-TEST_URL = 'https://www.hemato.io'
+TEST_URL = 'https://react-rails-playground.herokuapp.com'
 ROOT_URL = 'https://www.googleapis.com/pagespeedonline/v2/runPagespeed'
 
 def new_commit?
