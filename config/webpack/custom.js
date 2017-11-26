e35390ed98945d22e58b29c44b11c6348780c532
Adrien Poly <adrienpoly@gmail.com>
2017-11-26 04:48:57 +0000

    first try with purify but broken dropdown

--- config/webpack/custom.js
+++ config/webpack/custom.js
@@ -1,6 +1,12 @@
 const { env } = require('process')
 const isProduction = env.NODE_ENV === 'production'
 
+const path = require('path');
+// const glob = require('glob');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const PurifyCSSPlugin = require('purifycss-webpack');
+const glob = require('glob-all');
+
 module.exports = {
   resolve: {
     alias: {
@@ -11,8 +17,6 @@ module.exports = {
       // vue_resource: 'vue-resource/dist/vue-resource',
     }
   },
-
-
   module: {
     loaders: [
       {
@@ -26,5 +30,45 @@ module.exports = {
         ]
       }
     ]
-  }
-}
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].[contenthash].css'),
+    // Make sure this is after ExtractTextPlugin!
+    new PurifyCSSPlugin({
+      // Give paths to parse for rules. These should be absolute!
+      // paths: glob.sync(path.join(__dirname, '../../app/**/*.html.*')),
+      paths: glob.sync([
+        path.join(__dirname, '../../app/**/*.html.*'),
+        path.join(__dirname, '../../app/**/*.js*'),
+        path.join(__dirname, '../../app/**/*.json')
+      ]),
+      purifyOptions: {
+        minify: true,
+        info: true,
+        rejected: true
+      }
+    })
+  ]
+
+
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.css$/,
+  //       use: ExtractTextPlugin.extract({
+  //         // fallback: 'style-loader',
+  //         use: ['css-loader']
+  //       })
+  //     }
+  //   ]
+  // },
+  // plugins: [
+  //   new ExtractTextPlugin('[name].[contenthash].css'),
+  //   // Make sure this is after ExtractTextPlugin!
+  //   new PurifyCSSPlugin({
+  //     // Give paths to parse for rules. These should be absolute!
+  //     paths: glob.sync(path.join(__dirname, 'app/*.html')),
+  //   })
+  // ]
+};
+console.log(__dirname);
