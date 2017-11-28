cb0b49e9653d762a35d89e992c3f03044c8684e8
Adrien Poly <adrienpoly@gmail.com>
2017-11-28 02:38:34 +0000

    latest & best values are now part of the react component

--- app/javascript/page_speed_chart/chart.js
+++ app/javascript/page_speed_chart/chart.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import { LineChart, Line, Tooltip, YAxis, XAxis } from 'recharts';
+import { LineChart, Line, Tooltip, YAxis, XAxis, Legend } from 'recharts';
 import _ from 'lodash'
 import axios from 'axios'
 
@@ -8,6 +8,16 @@ import axios from 'axios'
 function mapData(data){
   if (!data) return
   return data.map((e) => { return {value: e.value, name: e.commit_id.substring(0,7)}})
+}
+
+function max(commits) {
+  if (!commits) return 0
+  return _.max(commits.map((commit) => commit.value))
+}
+
+function last(commits) {
+  if (!commits) return 0
+  return commits.pop().value
 }
 
 
@@ -29,9 +39,10 @@ export default class PageSpeedChart extends Component {
       .then( commits => this.setState({data: commits}) )
     }
 
-
     render() {
-      const data = mapData(this.state.data)
+      const data   = mapData(this.state.data)
+      const latest = last(this.state.data)
+      const best   = max(this.state.data)
         return (
           <div>
             <LineChart data={data} width={530} height={250}
@@ -49,8 +60,9 @@ export default class PageSpeedChart extends Component {
               <YAxis type="number" domain={[0, 100]} label={{ value: 'Score', angle: -90, position: 'insideLeft', offset: 15 }} />
               <XAxis dataKey="name" label={{ value: 'Commit', position: 'insideBottom', offset: -10 }}  />
               <Line type="monotone" dataKey="value" dot={false} isAnimationActive={false} stroke="url(#colorValue)" strokeWidth={3} />
-              <Tooltip cursor={false} isAnimationActive={false}/>
+              <Tooltip/>
             </LineChart>
+            <p className="text-muted text-center">Latest = {latest}, Best = {best} </p>
           </div>
         );
     }
