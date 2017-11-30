4bcf5bb5671c5bd7c399d6de500808eb0166b0b5
Adrien Poly <adrienpoly@gmail.com>
2017-11-30 00:39:41 +0000

    Sort commits desc

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -4,7 +4,7 @@ class CommitsController < ApplicationController
   # respond_to :json
 
   def index
-    @commits = Commit.all
+    @commits = Commit.all.order(release_created_at: :desc)
     render json: @commits
   end
 end
