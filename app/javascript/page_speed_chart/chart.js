02ac7de59fa0add4a5a9a7cffb11b7f70beadbce
Adrien Poly <adrienpoly@gmail.com>
2017-12-01 16:25:43 +0100

    design enhancement

--- app/javascript/page_speed_chart/chart.js
+++ app/javascript/page_speed_chart/chart.js
@@ -38,7 +38,7 @@ export default class PageSpeedChart extends Component {
         return (
           <div className="page-speed-app">
             <div className="page-speed-chart">
-              <ResponsiveContainer>
+              <ResponsiveContainer height={320}>
                 <BarChart data={data} margin={{top: 30, right: 30, bottom: 20, left: 0}} width={520} height={250}>
                   <YAxis type="number" domain={[0, 100]} label={{ value: 'Score', angle: -90, position: 'insideLeft', offset: 15 }} />
                   <XAxis dataKey="name" label={{ value: 'Commit', position: 'insideBottom', offset: -10 }}  />
