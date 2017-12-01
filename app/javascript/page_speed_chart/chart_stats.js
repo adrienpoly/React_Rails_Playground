d98aab519a30bcb00a7950f696692e086ebc712c
Adrien Poly <adrienpoly@gmail.com>
2017-12-01 16:40:17 +0100

    Correct bug for the last value computation

--- app/javascript/page_speed_chart/chart_stats.js
+++ app/javascript/page_speed_chart/chart_stats.js
@@ -7,27 +7,30 @@ function max(commits) {
 
 function last(commits) {
   if (!commits) return 0
-  return commits.pop().value
+
+  console.log('last', commits[-1]);
+  return commits[commits.length - 1].value
 }
 
 export default class ChartStats extends Component {
 
     render() {
+      console.log(this.props.data);
       const latest = last(this.props.data)
       const best   = max(this.props.data)
         return (
           <div className="row mt-3">
               <div className="col-sm-3 offset-sm-2 text-center align-middle">
-                <div class="card">
-                  <div class="card-header text-center">
+                <div className="card">
+                  <div className="card-header text-center">
                     Latest
                     <h5>{latest}</h5>
                   </div>
                 </div>
               </div>
               <div className="offset-sm-2 col-sm-3 offset-sm-2 text-center align-middle">
-                <div class="card">
-                  <div class="card-header text-center">
+                <div className="card">
+                  <div className="card-header text-center">
                     Best
                     <h5>{best}</h5>
                   </div>
