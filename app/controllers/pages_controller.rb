ba92aaf313dad45e10fa68ad00209f2088744a70
Adrien Poly <adrienpoly@gmail.com>
2017-12-03 01:56:08 +0100

    Page speed view added with 3 charts to be refined

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
+  include HighVoltage::StaticPage
+
   def home
     @scores = Commit.scores
     @latest = @scores.last.value
     @best   = Commit.best
+  end
+
+  def page_speed
+    @charts = Commit.charts
+    # Commit.attributes.each do |attribute|
+    #   @values[attribute] = { values: Commit.send(attribute) }
+    # end
   end
 end
